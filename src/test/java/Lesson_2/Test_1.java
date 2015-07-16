06ae0be51925e354dbcb9735250f9a991614f8b5
Dima Kyrychek <dimakirichek2012@gmail.com>
2015-07-16 10:46:31 +0300

    bionicProject

---
+++ src/test/java/Lesson_2/Test_1.java
@@ -0,0 +1,23 @@
+package Lesson_2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Hasler on 14.07.2015.
+ */
+public class Test_1 {
+
+    @Test
+    public void Example(){
+        Assert.assertTrue(isPrime(5),"This numner isn't prime number");
+    }
+
+    public boolean isPrime(int N){
+        if(N<=2) return true;
+        for (int i=2; i*i<=N;i++)
+            if (N%i==0)return false;
+        return true;
+    }
+
+}
