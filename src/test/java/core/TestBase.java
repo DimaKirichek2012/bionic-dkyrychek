0b4552cd82f1491a510398e35551b3ffb2f7c344
Dima <dimakirichek2012@gmail.com>
2015-08-03 21:01:36 +0300

    03.08.15 lesson

--- src/test/java/core/TestBase.java
+++ src/test/java/core/TestBase.java
@@ -6,7 +6,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import page.GoogleSearchPage;
+import ui_tests.TestData;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,14 +16,17 @@ import java.util.concurrent.TimeUnit;
  */
 public class TestBase{
 
-    protected WebDriver driver = new FirefoxDriver();
+    protected WebDriver driver;
     protected WebDriverWait webDriverWait;
 
     @BeforeSuite
-    public void setUp(){
-        webDriverWait = new WebDriverWait(driver,10);
+    public void setUp() throws IOException{
+        driver = WebDriverFactory.getWebDriver(TestData.BROWSER_NAME);
+        webDriverWait = new WebDriverWait(driver,50);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10,TimeUnit.SECONDS);
     }
 
     @AfterSuite
